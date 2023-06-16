bb5f5c00d0f7cde7c5029c0a1a034bd78a9411af
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-06-15 21:42:20 -0300

    update

---
+++ src/pages/Videos.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './videos.css'
+
+function Videos() {
+  return (
+    <div className="conteiner__videos">Videos legais
+        <video className='__video' src='https://www.tiktok.com/@paulosantos2058/video/6809257401062051078'>
+            
+        </video>
+    </div>
+  )
+}
+
+export default Videos
