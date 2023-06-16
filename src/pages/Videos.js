bb10730ba280583cc70a16423243381e68792eb5
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-06-16 01:08:35 -0300

    update add video

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import './videos.css'
 
 function Videos() {
+  const videoRef = useRef(null)
+  const [play, setPlay] = useState(false)
+
+  function handleStart(){
+    if(!play){
+      videoRef.current.play()
+      setPlay(true)
+    }else{
+      videoRef.current.pause()
+      setPlay(false)
+    }
+  }
   return (
-    <div className="conteiner__videos">Videos legais
-        <video className='__video' src='https://www.tiktok.com/@paulosantos2058/video/6809257401062051078'>
-            
+    <div className="conteiner__videos">
+        <video 
+          className='__video'
+          ref={videoRef}
+          onClick={handleStart}
+          loop
+          src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+        >
+
         </video>
     </div>
   )
